851b8d5daeb78f36af4dbe703713c6b8fdae1be1
Chris Davison <c.jr.davison@gmail.com>
2022-10-06 22:40:00 +0100

    Implement add, hard, repeat, and update

--- src/file.rs
+++ src/file.rs
@@ -1,60 +1,102 @@
 use anyhow::{anyhow, Error, Result};
 use chrono::prelude::*;
 
-#[derive(Debug, PartialEq)]
+const MODIFIER: f32 = 2.5;
+const MAX_INTERVAL: usize = 365 * 2;
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct SpacedTask {
     pub name: String,
     pub date: chrono::NaiveDate,
     pub interval: usize,
-    pub modifier: f32,
 }
 
-pub fn parse() -> Vec<SpacedTask> {
-    vec![]
+pub fn get_tasks(filename: &str) -> Result<Vec<SpacedTask>> {
+    let mut tasks = Vec::new();
+    let contents = std::fs::read_to_string(filename)?;
+    for line in contents.lines() {
+        if line.is_empty() {
+            continue;
+        }
+        tasks.push(line.parse::<SpacedTask>()?);
+    }
+    Ok(tasks)
+}
+
+pub fn write_tasks(tasks: &[SpacedTask], filename: &str) -> Result<()> {
+    std::fs::write(
+        filename,
+        tasks
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join("\n"),
+    )
+    .map(|_| ())
+    .map_err(|_| anyhow!("Failed to write tasks"))
 }
 
 impl std::str::FromStr for SpacedTask {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = s.split(",").clone().collect();
+        let parts: Vec<&str> = s.split(',').clone().collect();
         let name = parts[0];
-        dbg!(&parts[1]);
-        let dateparts = parts[1].split("-").collect::<Vec<&str>>();
+        let dateparts = parts[1].split('-').collect::<Vec<&str>>();
         let date = NaiveDate::from_ymd(
             dateparts[0].parse()?,
             dateparts[1].parse()?,
             dateparts[2].parse()?,
         );
-        let interval = interval_days(&parts[2])?;
-        let modifier = parts[3].parse()?;
+        let interval = parts[2].parse::<usize>()?;
         Ok(SpacedTask {
             name: name.to_string(),
             date,
             interval,
-            modifier,
         })
     }
 }
 
-fn interval_days(s: &str) -> Result<usize> {
-    let mut cur_num = String::new();
-    for ch in s.chars() {
-        if ch.is_numeric() {
-            cur_num.push(ch);
-            continue;
-        } else if ch == '-' {
-            return Err(anyhow!("Negative repeat interval"));
-        } else {
-            if cur_num.is_empty() {
-                return Err(anyhow!("No number"));
-            }
-            return cur_num
-                .parse()
-                .map_err(|_| anyhow!("Unrecognised number period {ch}"));
-        }
+impl SpacedTask {
+    pub fn new(title: String) -> SpacedTask {
+        let added = SpacedTask {
+            name: title,
+            date: Utc::now().date_naive(),
+            interval: 1,
+        };
+
+        println!("Created: {added}");
+        added
     }
-    Err(anyhow!("Didn't get proper format. Expected <usize>d"))
+
+    pub fn update(&mut self) {
+        self.interval = ((self.interval as f32 * MODIFIER).ceil() as usize).min(MAX_INTERVAL);
+        self.date += chrono::Duration::days(self.interval as i64);
+        println!("Updated: {self}")
+    }
+
+    pub fn repeat(&mut self) {
+        self.date += chrono::Duration::days(self.interval as i64);
+        println!("Repeated: {self}")
+    }
+
+    pub fn hard(&mut self) {
+        self.interval = ((self.interval as f32 / MODIFIER).ceil() as usize).min(MAX_INTERVAL);
+        self.date += chrono::Duration::days(self.interval as i64);
+        println!("Hard updated {self}")
+    }
+
+    pub fn reset(&mut self) {
+        self.interval = 1;
+        self.date += chrono::Duration::days(self.interval as i64);
+        println!("Reset {self}")
+    }
+}
+
+impl std::fmt::Display for SpacedTask {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{},{},{}", self.name, self.date, self.interval)
+    }
 }
 
 #[cfg(test)]
@@ -63,12 +105,11 @@ mod tests {
 
     #[test]
     fn parse_entry_test() {
-        let line = "guitar,2022-10-01,1d,2.5";
+        let line = "guitar,2022-10-01,1d";
         let want = SpacedTask {
             name: String::from("guitar"),
             date: Utc.ymd(2022, 10, 1).naive_utc(),
             interval: 1,
-            modifier: 2.5,
         };
         let got: SpacedTask = line.parse().unwrap();
         assert_eq!(got, want);
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,56 @@
 #![allow(dead_code, unused_variables, unused_imports)]
 mod file;
 
+use file::SpacedTask;
+
+use chrono::prelude::*;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Args {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(about = "space task revisits")]
+enum Command {
+    Add { title: String },
+    Update { index: usize },
+    Repeat { index: usize },
+    Hard { index: usize },
+    View,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::from_args();
+    let filename = "local.txt";
+    let mut tasks = match file::get_tasks(filename) {
+        Ok(t) => t,
+        Err(e) => {
+            eprintln!("{}", e);
+            return;
+        }
+    };
+    match args.cmd {
+        Command::Add { title } => add(&mut tasks, title),
+        Command::Hard { index } => tasks[index].hard(),
+        Command::Repeat { index } => tasks[index].repeat(),
+        Command::Update { index } => tasks[index].update(),
+        Command::View => view(&tasks),
+    }
+    if let Err(e) = file::write_tasks(&tasks, filename) {
+        eprintln!("{e}");
+    }
+}
+
+fn add(tasks: &mut Vec<SpacedTask>, title: String) {
+    let to_add = SpacedTask::new(title);
+    tasks.push(to_add)
+}
+
+fn view(tasks: &[SpacedTask]) {
+    for (i, t) in tasks.iter().enumerate() {
+        println!("{i:4}. {t}");
+    }
 }
